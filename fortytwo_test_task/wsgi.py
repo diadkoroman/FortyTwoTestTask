e6d56306eff2b7e78b665e25a16e567b7926f57c
Roman Shmatok <ramon1@ukr.net>
2015-10-29 13:53:41 +0200

    #1: далі пробую "завести" проект на getbarista
    Проект видає помилку 502, в логах - повідомлення, що не знайдено virtualenvironment. Тому пробую варіанти "підйому" далі.

--- fortytwo_test_task/wsgi.py
+++ fortytwo_test_task/wsgi.py
@@ -6,8 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
+import os,sys
 
-import os
+path="/d/diadkoroman.fortytwotesttask-247"
+if not path in sys.path:
+    sys.path.append(path)
+
+activate_this = "/d/diadkoroman.fortytwotesttask-247/django272/bin/activate_this.py"
+execfile(activate_this, dict(__file__=activate_this))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fortytwo_test_task.settings")
 
 from django.core.wsgi import get_wsgi_application
