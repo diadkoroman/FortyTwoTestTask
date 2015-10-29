eb630dd93f4774a785cab75471676b019644f7b0
Roman Shmatok <ramon1@ukr.net>
2015-10-29 08:37:17 +0200

    updated virtualenv path

--- cmd_activate.sh
+++ cmd_activate.sh
@@ -1,4 +1,4 @@
 #/bin/sh
-source django272/bin/activate
+source /d/diadkoroman.fortytwotesttask-247/django272/bin/activate
 uwsgi --http :8000 --module fortytwo_test_task.wsgi 
 #uwsgi --socket ../run/42test.sock --module ../fortytwo_test_task.wsgi --chmod-socket=664
